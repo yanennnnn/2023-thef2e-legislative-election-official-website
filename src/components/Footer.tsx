c5933a590528cbea2c0cbe8970846c9e2fa70ac4
yanenn <sh5ojkmt@gmail.com>
2023-11-17 21:43:19 +0800

    feat: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,62 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+const StyledFooter = styled.footer`
+  padding: 32px 40px;
+  border-top: 1px solid  #B9B9B9;
+  color: #595959;
+  font-size:15px;
+  .bottom {
+    font-size: 15px;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
+`
+const StyledLink = styled(Link)`
+  margin-left: 24px;
+  font-size: 15px;
+`;
+const routerLink = [
+  {
+    title: '首頁',
+    path: '/'
+  },
+  {
+    title: '認識候選人',
+    path: 'about'
+  },
+  {
+    title: '最新活動',
+    path: 'news'
+  },
+  {
+    title: '政治議題',
+    path: 'policy'
+  },
+  {
+    title: '小額捐款',
+    path: 'donate'
+  },
+]
+
 function Footer() {
 
+  
   return (
-    <>
-      Footer
-    </>
+    <StyledFooter>
+      <p>2023 鬥立翰 版權所有。</p>
+      <div className="bottom">
+        <p>辦公室地址｜毛孩區，毛茸茸大道99號，狗狗大厦99樓　Tel｜(02) 888-5678　Email｜dogoffice@doglihan.tw</p>
+        <div>
+          {routerLink.map((item) => (
+            <StyledLink key={item.title} to={item.path}>
+              {item.title}
+            </StyledLink>
+          ))}
+        </div>
+      </div>
+     
+    </StyledFooter>
   )
 }
 
